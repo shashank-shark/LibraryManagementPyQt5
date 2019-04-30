10a4682649e7452d7238fa3b7f2a234a282556c8
Shashank J <shashankreddy231@gmail.com>
2019-04-30 09:37:55 +0530

    Update index.py
    Connecting push buttons to Tabs

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+import MySQLdb
 import sys
 
 from PyQt5.uic import loadUiType
@@ -12,6 +13,30 @@ class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.Handle_Buttons()
+
+    def Handle_Buttons(self):
+        self.pushButton.clicked.connect(self.Open_Day_to_Day_Tab)
+        self.pushButton_2.clicked.connect(self.Open_Books_Tab)
+        self.pushButton_3.clicked.connect(self.Open_Users_Tab)
+        self.pushButton_4.clicked.connect(self.Open_Settings_Tab)
+
+    def Handle_UI_Changes(self):
+        pass
+
+    ##############  SHOW CURRENT TABS ##############
+
+    def Open_Day_to_Day_Tab(self):
+        self.tabWidget.setCurrentIndex(0)
+
+    def Open_Books_Tab(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def Open_Users_Tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Settings_Tab(self):
+        self.tabWidget.setCurrentIndex(3)
 
 
 def main():
